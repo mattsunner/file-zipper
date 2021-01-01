6733a7bba6af58ca6b38666f25adada72c2c01ea
Matt Sunner <matt@mattsunner.com>
2021-01-01 00:48:57 -0600

    Update Script to Module
    Update the script to a class based module.

--- file_manager/file_manager.py
+++ file_manager/file_manager.py
@@ -1,25 +1,39 @@
 import os
 from zipfile import ZipFile
-
-
-def zip_viewer(file_path):
-    with ZipFile(file_name, 'r') as zip:
+           
+            
+ class Zipper:
+ 	def __init__(self, file_path, destination_path=None, file_status=None, zip_path_name=None, file__paths_to_zip=None):
+ 		self.file_path = file_path
+ 		self.destination_path = destination_path
+ 		self.file_status = file_status
+ 		self.zip_path_name = zip_path_name
+ 		self.file_paths_to_zip = file__paths_to_zip
+ 		
+ 	def zip_viewer(self):
+    with ZipFile(self.file_name, 'r') as zip:
         zip.printdir()
-
-
-def file_unzipper(file_path, file_status, destination_path):
-    with ZipFile(file_path, file_status) as zip:
-        zip.extractall(path=destination_path)
-
-
-def file_zipper(file_path, zip_path_name):
+        
+ 	def file_unzipper(self.file_path, self.file_status, self.destination_path):
+    with ZipFile(self.file_path, self.file_status) as zip:
+        zip.extractall(path=self.destination_path)
+        
+  def file_zipper(self.file_path, self.zip_path_name):
     file_paths_to_zip = []
 
     for root, directories, files in os.walk(file_path):
         for filename in files:
             filepath = os.path.join(root, filename)
             file_paths_to_zip.append(filepath)
-
+            
     with ZipFile(zip_path_name, 'w') as zip:
         for file in file_paths_to_zip:
             zip.write(file)
+            
+ 
+ def main():
+ 	pass 
+ 	
+ 	
+ if __name__ == "__main__":
+ 	main()
